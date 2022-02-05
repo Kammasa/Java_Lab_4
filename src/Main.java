cd07acb7b7886ae517ee284b8a39454956d4fd66
kamil <masacz88@gmail.com>
2022-02-05 03:37:34 +0100

    Cw.4

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
@@ -21,6 +21,29 @@ public class Main {
            white(!isIndexCorrect) {}
         */
 
+         while(true) {
+             Scanner scanner = new Scanner(System.in);
+             System.out.println("Podaj liczbę od 0 do 100");
+             int[] tab = new int[100];
+             for (int i = 0; i <= 100; i++){}
+
+                 try {
+                     int elem = scanner.nextInt();
+                     try {
+                         int x = tab[elem];
+                         System.out.println("Wartość tego indeksu to: " + elem);
+                         return;
+                     } catch (ArrayIndexOutOfBoundsException e) {
+                         System.out.println("Liczba nie mieści się w przedziale");
+                     }
+                 } catch (InputMismatchException e) {
+                     System.out.println("Bład! nie wprowadzono liczby");
+                 }
+                 finally {
+                     System.out.println("koniec programu");
+                 }
+
+
 
         /* zad.2
         a) stworzyć dowolny przykład z NullPonterException,
@@ -28,5 +51,6 @@ public class Main {
         c) stworzyć dowolny przykład z 2 lub 3 blokami catch i z blokiem finally,
          */
 
+            }
+        }
     }
-}
